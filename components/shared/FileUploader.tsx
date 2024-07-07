9c12a326e2f779dcfabee6c4db91884b8e11f185
Antoine-Lee <antoinekllee@gmail.com>
2024-07-07 12:41:32 +0800

    Start creating react-hook-form with zod validation

---
+++ components/shared/FileUploader.tsx
@@ -0,0 +1,13 @@
+import React, { Dispatch, SetStateAction } from "react";
+
+type FileUploaderProps = {
+    imageUrl: string;
+    onFieldChange: (value: string) => void;
+    setFiles: Dispatch<SetStateAction<File[]>>;
+};
+
+const FileUploader = ({ imageUrl, onFieldChange, setFiles }: FileUploaderProps) => {
+    return <div>FileUploader</div>;
+};
+
+export default FileUploader;
