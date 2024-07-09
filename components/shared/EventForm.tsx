a9d14d2cd750eb257bf6e84c346fec8596e3957b
Antoine-Lee <antoinekllee@gmail.com>
2024-07-09 15:10:26 +0800

    Backend for creating new event, frontend for displaying event

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -14,10 +14,12 @@ import { Textarea } from "@/components/ui/textarea";
 import { FileUploader } from "./FileUploader";
 import { useState } from "react";
 import Image from "next/image";
-
+import { useUploadThing } from "@/lib/uploadthing";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Checkbox } from "../ui/checkbox";
+import { useRouter } from "next/navigation";
+import { createEvent } from "@/lib/actions/event.actions";
 
 type EventFormProps = {
     userId: string;
@@ -25,8 +27,12 @@ type EventFormProps = {
 };
 
 const EventForm = ({ userId, type }: EventFormProps) => {
-    const initialValues = eventDefaultValues;
     const [files, setFiles] = useState<File[]>([]); 
+    
+    const initialValues = eventDefaultValues;
+    const router = useRouter(); 
+
+    const { startUpload } = useUploadThing('imageUploader');
 
     // 1. Define your form.
     const form = useForm<z.infer<typeof eventFormSchema>>({
@@ -35,10 +41,42 @@ const EventForm = ({ userId, type }: EventFormProps) => {
     });
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    async function onSubmit(values: z.infer<typeof eventFormSchema>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         console.log(values);
+
+        const eventData = values; 
+
+        let uploadedImageUrl = values.imageUrl; 
+
+        if (files.length > 0) {
+            const uploadedImages = await startUpload(files); 
+
+            if (!uploadedImages)
+                return; 
+
+            uploadedImageUrl = uploadedImages[0].url;
+        }
+
+        if (type === "Create") {
+            try
+            {
+                const newEvent = await createEvent({
+                    event: { ...values, imageUrl: uploadedImageUrl },
+                    userId, 
+                    path: '/profile'
+                }); 
+
+                if (newEvent)
+                {
+                    form.reset(); 
+                    router.push('/events/${newEvent._id}');
+                }
+            } catch (error) {
+                console.log (error);   
+            }
+        }
     }
 
     return (
