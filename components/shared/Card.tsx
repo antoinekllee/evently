c1a6f2496c0a06c504b9d8c0cff90b7945238007
Antoine-Lee <antoinekllee@gmail.com>
2024-07-09 22:20:45 +0800

    Profile page, fix hydration error in Card.tsx

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -44,10 +44,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) =>
                 </div>
             )}
 
-            <Link
-                href={`/events/${event._id}`}
-                className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
-            >
+            <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
                 { !hidePrice && <div className='flex gap-2'>
                     <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
                         {event.isFree ? 'Free' : `$${event.price}`}
@@ -60,8 +57,10 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) =>
                 <p className="p-medium-16 p-medium-18 text-grey-500">
                     {formatDateTime(event.startDateTime).dateTime}
                 </p>
-
-                <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">{event.title}</p>
+                
+                <Link href={`/events/${event._id}`}>
+                    <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">{event.title}</p>
+                </Link>
 
                 <div className="flex-between w-full">
                     <p className="p-medium-14 md:p-medium-16 text-grey-600">
@@ -84,7 +83,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) =>
                     )}
 
                 </div>
-            </Link>
+            </div>
                 
         </div>
     )
