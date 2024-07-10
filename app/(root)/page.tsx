684080e8bd77d54f76fbf4f3d275695af3986da4
Antoine-Lee <antoinekllee@gmail.com>
2024-07-10 15:49:03 +0800

    Final pagination

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -68,8 +68,8 @@ export default async function Home({ searchParams }: SearchParamProps) // how do
                         emptyStateSubtext="Come back later"
                         collectionType="All_Events"
                         limit={6}
-                        page={1}
-                        totalPages={2}
+                        page={page}
+                        totalPages={events?.totalPages}
                     />
                 </section>
             </>
